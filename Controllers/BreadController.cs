d3e74341433440664a6c3c41858a08f9e56c7f2a
OneTrueJASH <jash@hails8n.com>
2021-04-22 18:25:51 -0600

    Building stuff out

---
+++ Controllers/BreadController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TheRobeOfBreadSummoning.API.Domain.Models;
+
+namespace TheRobeOfBreadSummoning.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BreadController : ControllerBase
+    {
+
+        private readonly ILogger<BreadController> _logger;
+
+        public BreadController(ILogger<BreadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Ok(Bread.Loaves);
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(int id)
+        {
+            return Ok(Bread.Loaves[id]);
+        }
+    }
+}
